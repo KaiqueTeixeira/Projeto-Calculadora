77acc20cdab9a8bcccec8cda37b1867b17752900
Kaíque <158008913+KaiqueTeixeira@users.noreply.github.com>
2025-05-23 17:13:39 -0300

    Update Scrpit

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let resultado = document.getElementById("resultado")
-let currentInput = ""
+let currentInput = "0"
 let currentOperator = ""
 
+resultado.innerHTML = currentInput
+
 function insert(data) {
-    if (currentInput.length < 15){
+    if (currentInput == "0") {
+        currentInput = data
+    }
+    else if (currentInput.length < 15){
         currentInput += data
         resultado.innerHTML = currentInput
     } else {
         currentInput += ""
         resultado.innerHTML = currentInput
     }
+
+    resultado.innerHTML = currentInput
 }
 
 function insertOperator(operator) {
-    if (currentInput === "" || operator == ".") return
+    if (currentInput === "") return
+
+    const sinal = currentInput.slice(-1)
+    if ("-+*/".includes(sinal)) return
+
     currentInput += operator
     resultado.innerHTML = currentInput
 }
@@ -21,20 +32,28 @@ function insertOperator(operator) {
 /* Botão de Calcular "=" */
 function calcular() {
     try{
-        resultado.innerHTML = math.evaluate(resultado.innerHTML)
+        let conta = math.evaluate(currentInput)
+        resultado.innerHTML = conta
+        currentInput = conta
     } catch {
         resultado.innerHTML = "Error"
+        currentInput = "0"
     }
 }
 
 /* Botão de Apagar "C" */
 function limpar() {
-    currentInput = ""
+    currentInput = "0"
     resultado.innerHTML = currentInput
 }
 
 /* Botão de Apagar "<" */
 function apagar() {
-    currentInput = currentInput.slice(0, -1)
+    if (currentInput.length <= 1) {
+        currentInput = "0"
+    } else {
+        currentInput = currentInput.slice(0, -1)
+    }
+    
     resultado.innerHTML = currentInput
 }
